9ab8c0ac56d50e5699e61c096e28a0a5371472d4
Andre Candido <andre.eng1313@gmail.com>
2022-10-17 16:38:12 -0300

    Alteração para colcoar duas casas decimais com tofixed.

--- driven_eats.js
+++ driven_eats.js
@@ -12,6 +12,10 @@ let preco1
 let preco2
 
 
+
+
+
+
 function escolherfrango1 () {
 
     document.getElementById("frangotop1").style.borderColor = "#32b72f"
@@ -19,9 +23,10 @@ function escolherfrango1 () {
     document.getElementById("frangotop3").style.borderColor = "white"
 
  
-    prato = "- Prato: Frango yin-yang"
+    prato = "- Prato: Espetinhos"
 
-    preco = 10  
+    preco = 14.90 
+  
     
 }
 
@@ -32,9 +37,10 @@ function escolherfrango2 () {
     document.getElementById("frangotop2").style.borderColor = "#32b72f"
     document.getElementById("frangotop3").style.borderColor = "white"
     
-    prato = "- Prato: Frango yin-yang"
+    prato = "- Prato: Hamburgão"
 
-    preco = 2
+    preco = 20.90
+  
     
     }
 
@@ -46,9 +52,10 @@ function escolherfrango3 () {
     document.getElementById("frangotop2").style.borderColor = "white"
     document.getElementById("frangotop3").style.borderColor = "#32b72f"
     
-    prato = "- Prato: Frangão"
+    prato = "- Prato: Frango Yin Yang"
 
-    preco = 5
+    preco = 33.90
+   
 
     }
 
@@ -59,9 +66,10 @@ function escolherbebida1 () {
     document.getElementById("bebidatop2").style.borderColor = "white"
     document.getElementById("bebidatop3").style.borderColor = "white"
     
-    bebida = "- Bebida: Red Bull"
+    bebida = "- Bebida: Coke"
 
-    preco1 = 7
+    preco1 = 5.90
+    
     
 }
 
@@ -73,9 +81,10 @@ function escolherbebida2 () {
     document.getElementById("bebidatop2").style.borderColor = "#32b72f"
     document.getElementById("bebidatop3").style.borderColor = "white"
     
-    bebida = "- Bebida: coquinha"
+    bebida = "- Bebida: Red Bull"
 
-    preco1 = 8
+    preco1 = 13
+   
     
     }
 
@@ -87,8 +96,10 @@ function escolherbebida3 () {
     document.getElementById("bebidatop2").style.borderColor = "white"
     document.getElementById("bebidatop3").style.borderColor = "#32b72f"
     
-    bebida = "- Bebida: coquinha"
-    preco1 = 11
+    bebida = "- Bebida: Capuccino"
+
+    preco1 = 8.50
+   
     
     }
     
@@ -99,7 +110,9 @@ function escolhersobremesa1 () {
     document.getElementById("mesatop3").style.borderColor = "white"
     
     sobremesa = "- Sobremesa: Tridents melancia"
-    preco2 = 14
+
+    preco2 = 2
+   
     
 }
 
@@ -111,8 +124,10 @@ function escolhersobremesa2 () {
     document.getElementById("mesatop2").style.borderColor = "#32b72f"
     document.getElementById("mesatop3").style.borderColor = "white"
 
-    sobremesa = "- Sobremesa: pudim"
-    preco2 = 18
+    sobremesa = "- Sobremesa: Cornetto"
+
+    preco2 = 7
+  
     
     
     }
@@ -125,15 +140,20 @@ function escolhersobremesa3 () {
     document.getElementById("mesatop2").style.borderColor = "white"
     document.getElementById("mesatop3").style.borderColor = "#32b72f"
     
-    sobremesa ="- Sobremesa: pudim"
-    preco2 = 111
-    
+    sobremesa ="- Sobremesa: Frutily"
+
+    preco2 = 5
+   
 }
+
 
 
   function finalizarpedido () {
 
-    let mensagem = "Olá, gostaria de fazer de fazer o pedido:" + "\n" + prato + "\n" + bebida + "\n" + sobremesa + "\n" + "Total: R$ " + (preco + preco1 + preco2);
+    soma = (preco + preco1 + preco2)
+    soma2casasdecimais = parseFloat(soma).toFixed(2);
+
+    let mensagem = "Olá, gostaria de fazer de fazer o pedido:" + "\n" + prato + "\n" + bebida + "\n" + sobremesa + "\n" + "Total: R$ " +  soma2casasdecimais;
 
     alert (mensagem)
     mensagem = window.encodeURIComponent(mensagem)
